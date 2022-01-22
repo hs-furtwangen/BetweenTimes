cba55a70b8fc5d357144337dd1c352a4b07a2eb1
GenitalerSound <44004425+GenitalerSound@users.noreply.github.com>
2022-01-22 16:42:12 +0100

    Inventory Update
    - Added better network control

--- BetweenTimes/Assets/Scripts/BetweenTime/Inventory/Network/BTNetworkInventory.cs
+++ BetweenTimes/Assets/Scripts/BetweenTime/Inventory/Network/BTNetworkInventory.cs
@@ -6,84 +6,99 @@ using UnityEngine;
 
 namespace StarterAssets.BetweenTime.Inventory.Network
 {
+    
     public class BTNetworkInventory : NetworkBehaviour
     {
         [SerializeField] protected List<Item> _inventory = new List<Item>();
         [SyncVar] public int maxItems = 1;
-        
-        [Header("Debug")] 
-        [SerializeField] private bool showDebug;
+
+        [Header("Debug")] [SerializeField] private bool showDebug;
         [SerializeField] private Color debugColor;
-        
+
         [Client]
         public void AddItem(Item itemToAdd)
         {
             if (!isLocalPlayer)
                 return;
-            
+
             CmdAddItem(itemToAdd);
         }
-    
+
         [Command]
         public void CmdAddItem(Item itemToAdd)
-        {            
+        {
+            ServerAddItemLogic(itemToAdd);
+        }
+        
+        [Server]
+        public void ServerAddItemLogic(Item itemToAdd)
+        {
             if (_inventory.Count >= maxItems)
             {
-                DebugColored.Log(true, Color.yellow,"[Server]", this, "Inventory full!");
+                DebugColored.Log(true, Color.yellow, "[Server]", this, "Inventory full!");
                 RpcAddItem(false, itemToAdd);
                 return;
             }
-            
+
             _inventory.Add(itemToAdd);
-            DebugColored.Log(true, Color.yellow,"[Server]", this, "Added Item "+itemToAdd.ToString());
+            DebugColored.Log(true, Color.yellow, "[Server]", this, "Added Item " + itemToAdd.ToString());
+
             RpcAddItem(true, itemToAdd);
         }
-        
+
         [ClientRpc]
         public void RpcAddItem(bool success, Item itemToAdd)
         {
             if (success)
             {
-                _inventory.Add(itemToAdd);
+                if (!(isClient && isServer))
+                    _inventory.Add(itemToAdd);
                 DebugColored.Log(true, Color.yellow, "[Rpc]", this, "Added Item " + itemToAdd.ToString());
-            }else
+            }
+            else
                 DebugColored.Log(true, Color.yellow, "[Rpc]", this, "Failed to add Item " + itemToAdd.ToString());
-
         }
-                
+
         [Client]
         public void RemoveItem(Item itemToRemove)
         {
-            if (!isLocalPlayer)
+            if (!isLocalPlayer || !isServer)
                 return;
             
-
-            CmdAddItem(itemToRemove);
+            CmdRemoveItem(itemToRemove);
         }
 
         [Command]
         public void CmdRemoveItem(Item itemToRemove)
-        {           
+        {
+            ServerRemoveItemLogic(itemToRemove);
+        }
+
+        [Server]
+        public void ServerRemoveItemLogic(Item itemToRemove)
+        {
             if (_inventory.Count <= 0)
             {
-                DebugColored.Log(showDebug,debugColor,this,"Inventory empty!");   
+                DebugColored.Log(showDebug, debugColor, this, "Inventory empty!");
                 return;
             }
-            
-            if (_inventory.Contains(itemToRemove))
+
+            lock (_inventory)
             {
-                bool success = _inventory.Remove(itemToRemove);
-                DebugColored.Log(showDebug,debugColor,this,"Removed "+itemToRemove.ToString());
-                if (success)
+                for (int i = 0; i < _inventory.Count; i++)
                 {
-                    RpcCmdRemoveItem(true, itemToRemove);
-                }else
-                    RpcCmdRemoveItem(false, itemToRemove);
-                return ;
+                    if (_inventory[i].id == itemToRemove.id)
+                    {
+                        _inventory.RemoveAt(i);
+                        DebugColored.Log(showDebug, debugColor, this, "Removed " + itemToRemove.ToString());
+                        RpcCmdRemoveItem(true, itemToRemove);
+                        return;
+                    }
+                }
             }
-           
+
             RpcCmdRemoveItem(false, itemToRemove);
-            DebugColored.Log(true, Color.yellow,"[Server]", this, "Failed to remove Item "+itemToRemove.ToString());
+            DebugColored.Log(true, Color.yellow, "[Server]", this, "Failed to remove Item " + itemToRemove.ToString());
         }
 
         [ClientRpc]
@@ -91,27 +106,50 @@ namespace StarterAssets.BetweenTime.Inventory.Network
         {
             if (success)
             {
-                _inventory.Remove(itemToRemove);
+                if (!(isClient && isServer))
+                    lock (_inventory)
+                    {
+                        for (int i = 0; i < _inventory.Count; i++)
+                        {
+                            if (_inventory[i].id == itemToRemove.id)
+                            {
+                                _inventory.RemoveAt(i);
+                                DebugColored.Log(showDebug, debugColor, this, "Removed " + itemToRemove.ToString());
+                                return;
+                            }
+                        }
+                    }
+                
                 DebugColored.Log(true, Color.yellow, "[Client]", this,
                     "Removed Item " + itemToRemove.ToString());
             }
             else
             {
                 DebugColored.Log(true, Color.yellow, "[Client]", this,
-                    "Failed to remove Item " + itemToRemove.ToString());   
+                    "Failed to remove Item " + itemToRemove.ToString());
             }
         }
-        
+
+        [Client]
         public void ClearInventory()
         {
+            if (!isLocalPlayer)
+                return;
+            
             CmdClearInventory();
         }
 
         [Command]
         public void CmdClearInventory()
         {
+            ServerClearInventoryLogic();
+        }
+
+        [Server]
+        public void ServerClearInventoryLogic()
+        {
             _inventory.Clear();
-            DebugColored.Log(true, Color.yellow,"[Server]", this, "Cleared Inventory" );
+            DebugColored.Log(true, Color.yellow, "[Server]", this, "Cleared Inventory");
             RpcInventory();
         }
 
@@ -119,8 +157,7 @@ namespace StarterAssets.BetweenTime.Inventory.Network
         public void RpcInventory()
         {
             _inventory.Clear();
-            DebugColored.Log(true, Color.yellow,"[Client]", this, "Cleared Inventory" );
-
+            DebugColored.Log(true, Color.yellow, "[Client]", this, "Cleared Inventory");
         }
     }
 }
