390972eb12dfbee0230704b952b35f04905bf817
MarcLohfink <loma@hs-furtwangen.de>
2022-01-23 21:02:57 +0100

    Light

--- BetweenTimes/Assets/Scripts/BetweenTime/Network/BTCustomNetworkManager.cs
+++ BetweenTimes/Assets/Scripts/BetweenTime/Network/BTCustomNetworkManager.cs
@@ -1,9 +1,16 @@
 using Mirror;
+using UnityEngine;
 
 namespace BetweenTime.Network
 {
     public class BTCustomNetworkManager : NetworkManager
     {
 
+
+        [ContextMenu("Start Client")]
+        public void TestStart()
+        {
+            StartClient();
+        }
     }
 }
